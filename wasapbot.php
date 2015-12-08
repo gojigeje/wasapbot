3f8cb839454d9c574bc72713fbcdc95cf157172e
Ghozy Arif Fajri <gojigeje@gmail.com>
2015-12-08 08:46:21 +0700

    update + clean code, hapus yang gak berguna :)

--- wasapbot.php
+++ wasapbot.php
@@ -26,13 +26,8 @@
   // --> whatsapp/examples/registerTool.php
 
   $nickname = "wasapbot";                          // bot nickname
-  $username = "62xxxxxxxxxxx";                     // bot number
-  $password = "C34G7Cauuw8lbxxxxxxxxxxxxxxxx";     // password
-  
-  $admin = array(                                  // bot admin number
-    "6285xxxxx6021",
-    "6285xxxxx6022"
-  );
+  // $username = "62xxxxxxxxxxx";                     // bot number
+  // $password = "C34G7Cauuw8lbxxxxxxxxxxxxxxxx";     // password
 
 
   // --------------------------------------------------------------------------------------------------------------- //
@@ -65,24 +60,9 @@
   // list semua event -->  https://github.com/WHAnonymous/Chat-API/wiki/WhatsAPI-Documentation#list-of-all-events
   $w->eventManager()->bind("onConnect", "onConnect");
   $w->eventManager()->bind("onDisconnect", "onDisconnect");
-  $w->eventManager()->bind("onPresence", "onPresence");
   $w->eventManager()->bind("onClose", "onClose");
   $w->eventManager()->bind("onGetMessage", "onGetMessage");
-  $w->eventManager()->bind("onGetAudio", "onGetAudio");
-  $w->eventManager()->bind("onGetImage", "onGetImage");
-  $w->eventManager()->bind("onGetLocation", "onGetLocation");
-  $w->eventManager()->bind("onGetVideo", "onGetVideo");
-  $w->eventManager()->bind("onGetvCard", "onGetvCard");
   $w->eventManager()->bind("onGetGroupMessage", "onGetGroupMessage");
-  $w->eventManager()->bind("onGetGroupImage", "onGetGroupImage");
-  $w->eventManager()->bind("onGetGroupVideo", "onGetGroupVideo");
-  $w->eventManager()->bind("onGroupCreate", "onGroupCreate");
-  $w->eventManager()->bind("onGroupisCreated", "onGroupisCreated");
-  $w->eventManager()->bind("onGroupsChatCreate", "onGroupsChatCreate");
-  $w->eventManager()->bind("onGroupsChatEnd", "onGroupsChatEnd");
-  $w->eventManager()->bind("onGroupsParticipantsAdd", "onGroupsParticipantsAdd");
-  $w->eventManager()->bind("onGroupsParticipantsRemove", "onGroupsParticipantsRemove");
-  $w->eventManager()->bind("onGetGroupV2Info", "onGetGroupV2Info");
 
   // konek ------------------------------------------------------------------------------------------------------------
   sleep(3);
@@ -96,68 +76,41 @@
   // poll message loop ------------------------------------------------------------------------------------------------
   while (1) {
 
-    if ($poll==10) {
+    if ($poll==5) {
       echo "\n[$ke][".date('H:i:s')."] ----------------------------------------------------\n";
-      echo "[$ke][".date('H:i:s')."] --- bot siap!\n";
+      echo "[$ke][".date('H:i:s')."] --- BOT SIAP!\n";
+      echo "[$ke][".date('H:i:s')."] --- \n";
+      echo "[$ke][".date('H:i:s')."] --- Sekarang coba kirim pesan ke bot ini,\n";
+      echo "[$ke][".date('H:i:s')."] --- seharusnya bot akan mengirim balik teks yang\n";
+      echo "[$ke][".date('H:i:s')."] --- dikirimkan ke dia.\n";
+      echo "[$ke][".date('H:i:s')."] --- \n";
+      echo "[$ke][".date('H:i:s')."] --- Ubah perilaku bot dengan mengedit function\n";
+      echo "[$ke][".date('H:i:s')."] --- onGetMessage() dan onGetGroupMessage()\n";
+      echo "[$ke][".date('H:i:s')."] --- di baris 126 dan baris 182.\n";
+      echo "[$ke][".date('H:i:s')."] --- \n";
+      echo "[$ke][".date('H:i:s')."] --- Semoga sukses!\n";
       echo "[$ke][".date('H:i:s')."] ----------------------------------------------------\n";
     }
 
-    // $w->sendPing();
     $w->pollMessage(true); // markAsRead
 
     // cek konek
-    if ($poll % 300 == 0 && $poll != 0) {
+    if ($poll % 200 == 0 && $poll != 0) {
       cek_konek();
     }
 
     $poll++; // poll control
   }
 
-  // functions --------------------------------------------------------------------------------------------------------
-  function onConnect($mynumber, $socket)
-  {
-    global $ke;
-    echo "[$ke][".date('H:i:s')."] - $mynumber sukses login!!\n";
-    echo "[$ke][".date('H:i:s')."] ----------------------------------------------------\n";
-  }
 
-  function onDisconnect($mynumber, $socket)
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] - $mynumber koneksi terputus!\n";
-    exit(1);
-  }
-
-  function onClose( $mynumber, $error )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onClose\n";
-    echo " mynumber : $mynumber\n";
-    echo " error    : $error\n\n";
-  }
-
-  function cek_konek() { 
-    global $ke;
-    $connected = @fsockopen("www.google.com", 80); 
-    if ($connected){
-      fclose($connected);
-    } else {
-      echo "\n[$ke][".date('H:i:s')."] Tidak bisa mengakses internet! coba lagi dalam 15 detik..\n";
-      sleep(15);
-      $connected2 = @fsockopen("www.google.com", 80); 
-      if ($connected2) {
-        fclose($connected2);
-      } else {
-        echo "\n[$ke][".date('H:i:s')."] EROR: Tidak bisa mengakses internet!\n";
-        exit(1);
-      }
-    }
-  }
+  // ------------------------------------------------------------------------------------------------------------------
+  // FUNCTIONS 
+  // ------------------------------------------------------------------------------------------------------------------
 
   // ketika dapat pesan pm
   function onGetMessage($mynumber, $from, $id, $type, $time, $name, $body)
   {
-    global $ke, $w;
+    global $ke, $w, $poll;
     $bodi = str_replace( array("\n", "\r\n", "\r") , " ", $body);
     $from = str_replace(array("@s.whatsapp.net","@g.us"), "", $from);  // nomer user
     $user = explode(' ',trim($name)); $nama = $user[0];                // ambil nama depan
@@ -174,46 +127,51 @@
     echo "name      : $name\n";
     echo "body      : $body\n";
 
-    // dari sini udah bisa bikin logic botnya..
-    // misalnya nge-echo balik apa chat yg dikirim ke botnya
-    // --------------------------------------------------------------------------------------------
 
-          // kirim typing..
-          $w->sendMessageComposing($from);
-          // selalu kasih jeda barang 2-3 detik, biar ga dikira bot
-          sleep(3);
-          // kirim balik
-          $w->sendMessage($from, $body);
-          // setelah send apapun, selalu poll
-          $w->pollMessage();
+    if ($poll > 5) { // abaikan poll-poll awal, biar nggak ngeflood
+
+      // dari sini udah bisa bikin logic botnya..
+      // misalnya nge-echo balik apa chat yg dikirim ke botnya
+      // --------------------------------------------------------------------------------------------
+
+            // kirim typing..
+            $w->sendMessageComposing($from);
+            // selalu kasih jeda barang 2-3 detik, biar ga dikira bot
+            sleep(3);
+            // kirim balik
+            $w->sendMessage($from, $body);
+            // setelah send apapun, selalu poll
+            $w->pollMessage();
 
 
-    // atau nge-respon perintah tertentu
-    // --------------------------------------------------------------------------------------------
+      // atau nge-respon perintah tertentu
+      // --------------------------------------------------------------------------------------------
 
-    //      // sesuaikan responnya dulu
-    //      if ($body == "!ping") {
-    //        $respon = "pong! $nama";
-    //      }
-    //      elseif ($body == "!help") {
-    //        $respon = "ada yang bisa dibantu, bos $nama? 😎";
-    //      }
-    //    
-    //      // jika ada respon, kirimkan responnya
-    //      if (!empty($respon)) {
-    //        // dikirim  belakangan
-    //        $w->sendMessageComposing($from_group); // kirim ke grup
-    //        sleep(3);
-    //        $w->sendMessage($from_group, $respon);
-    //        $w->pollMessage();
-    //      }
+      //      // sesuaikan responnya dulu
+      //      if ($body == "!ping") {
+      //        $respon = "pong! $nama";
+      //      }
+      //      elseif ($body == "!help") {
+      //        $respon = "ada yang bisa dibantu, bos $nama? 😎";
+      //      }
+      //    
+      //      // jika ada respon, kirimkan responnya
+      //      if (!empty($respon)) {
+      //        // dikirim  belakangan
+      //        $w->sendMessageComposing($from_group); // kirim ke grup
+      //        sleep(3);
+      //        $w->sendMessage($from_group, $respon);
+      //        $w->pollMessage();
+      //      }
+
+    }
 
   }
 
   // pesan di grup
   function onGetGroupMessage($mynumber, $from_group_jid, $from_user_jid, $id, $type, $time, $name, $body)
   {
-    global $ke, $w, $poll, $admin;
+    global $ke, $w, $poll;
     $from_group = str_replace(array("@s.whatsapp.net","@g.us"), "", $from_group_jid);     // id grup
     $from_user = str_replace(array("@s.whatsapp.net","@g.us"), "", $from_user_jid);       // nomer user
     $bodi = str_replace( array("\n", "\r\n", "\r") , " ", $body);
@@ -235,7 +193,7 @@
     echo "name            : $name\n";
     echo "body            : $body\n";
 
-    if ($poll > 10) { // abaikan poll-poll awal, biar nggak ngeflood di grup
+    if ($poll > 5) { // abaikan poll-poll awal, biar nggak ngeflood di grup
 
       // dari sini udah bisa bikin logic botnya..
       // misalnya nge-echo balik apa chat yg dikirim grup sama orang lain
@@ -275,105 +233,45 @@
 
   }
 
-
-  // ------------------------------------------------------------------------------------------------------------------
-  //  fungsi di bawah masih dianggurin,, utak-atik sendiri ya
-  // ------------------------------------------------------------------------------------------------------------------
-
-  function onGetGroupImage( $mynumber, $from_group_jid, $from_user_jid, $id, $type, $time, $name, $size, $url, $file, $mimeType, $fileHash, $width, $height, $preview, $caption )
+  function onConnect($mynumber, $socket)
   {
     global $ke;
-    echo "\n[$ke][".date('H:i:s')."] [$from_group_jid]\n";
-    echo "--- $name > [image] $caption $url\n";
+    echo "[$ke][".date('H:i:s')."] - $mynumber sukses login!!\n";
+    echo "[$ke][".date('H:i:s')."] - Tunggu hingga bot siap!\n";
+    echo "[$ke][".date('H:i:s')."] ----------------------------------------------------\n";
   }
 
-  function onGetAudio( $mynumber, $from, $id, $type, $time, $name, $size, $url, $file, $mimeType, $fileHash, $duration, $acodec )
+  function onDisconnect($mynumber, $socket)
   {
     global $ke;
-    echo "\n[$ke][".date('H:i:s')."] [$from]\n";
-    echo "--- $name > [audio] $url\n";
+    echo "\n[$ke][".date('H:i:s')."] - $mynumber koneksi terputus!\n";
+    exit(1);
   }
 
-  function onGetImage( $mynumber, $from, $id, $type, $time, $name, $size, $url, $file, $mimeType, $fileHash, $width, $height, $preview, $caption )
+  function onClose( $mynumber, $error )
   {
     global $ke;
-    echo "\n[$ke][".date('H:i:s')."] [$from]\n";
-    echo "--- $name > [image] $url\n";
-  }  
-
-  function onGetLocation( $mynumber, $from, $id, $type, $time, $name, $nameLocation, $longitude, $latitude, $url, $preview )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] [$from]\n";
-    echo "--- $name > [location] lat: $latitude lon: $longitude , $nameLocation\n";
+    echo "\n[$ke][".date('H:i:s')."] EVENT: onClose\n";
+    echo " mynumber : $mynumber\n";
+    echo " error    : $error\n\n";
   }
 
-  function onGetVideo( $mynumber, $from, $id, $type, $time, $name, $url, $file, $size, $mimeType, $fileHash, $duration, $vcodec, $acodec, $preview, $caption )
-  {
+  function cek_konek() { 
     global $ke;
-    echo "\n[$ke][".date('H:i:s')."] [$from]\n";
-    echo "--- $name > [video] $caption $url - duration:$duration, size: $size\n";
+    $connected = @fsockopen("www.google.com", 80); 
+    if ($connected){
+      fclose($connected);
+    } else {
+      echo "\n[$ke][".date('H:i:s')."] Tidak bisa mengakses internet! coba lagi dalam 15 detik..\n";
+      sleep(15);
+      $connected2 = @fsockopen("www.google.com", 80); 
+      if ($connected2) {
+        fclose($connected2);
+      } else {
+        echo "\n[$ke][".date('H:i:s')."] EROR: Tidak bisa mengakses internet!\n";
+        exit(1);
+      }
+    }
   }
 
-  function onGetGroupVideo( $mynumber, $from_group_jid, $from_user_jid, $id, $type, $time, $name, $url, $file, $size, $mimeType, $fileHash, $duration, $vcodec, $acodec, $preview, $caption )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] [$from_group_jid]\n";
-    echo "--- $name > [video] $caption $url - duration:$duration, size: $size\n";
-  }
-
-  function onGetvCard( $mynumber, $from, $id, $type, $time, $name, $vcardname, $vcard )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] [$from]\n";
-    echo "--- $name > [vcard] $vcardname\n";
-  }
-
-  function onPresence( $mynumber, $from, $status )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onPresence -> $from : $status\n\n";
-  }
-
-  function onGroupCreate( $mynumber, $groupId )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onGroupCreate -> $mynumber, $groupId \n\n";
-  }
-
-  function onGroupisCreated( $mynumber, $creator, $gid, $subject, $admin, $creation, $members = array()){}
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onGroupisCreated -> $mynumber, $creator, $gid, $subject, $admin, $creation \n\n";
-  }
-
-  function onGroupsChatCreate( $mynumber, $gid )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onGroupsChatCreate -> $mynumber, $gid \n\n";
-  }
-
-  function onGroupsChatEnd( $mynumber, $gid )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onGroupsChatEnd -> $mynumber, $gid \n\n";
-  }
-
-  function onGroupsParticipantsAdd( $mynumber, $groupId, $jid )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onGroupsParticipantsAdd -> $mynumber, $groupId, $jid \n\n";
-  }
-
-  function onGroupsParticipantsRemove( $mynumber, $groupId, $jid )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onGroupsParticipantsRemove -> $mynumber, $groupId, $jid \n\n";
-  }
-
-  function onGetGroupV2Info( $mynumber, $group_id, $creator, $creation, $subject, $participants, $admins, $fromGetGroup )
-  {
-    global $ke;
-    echo "\n[$ke][".date('H:i:s')."] EVENT: onGetGroupV2Info -> $mynumber, $group_id, $creator, $creation, $subject, $participants, $admins, $fromGetGroup \n\n"; 
-  }
  ?>
