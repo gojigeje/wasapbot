7feda55cc5233c54f78a200719450c5559fbbc3f
Ghozy Arif Fajri <gojigeje@gmail.com>
2016-01-07 00:30:50 +0700

    add bash laucher script

---
+++ wasapbot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# contoh untuk menjalanjan bot dari script bash
+#
+
+# cd ke folder script ini
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd "$DIR"
+
+# jalankan bot
+php wasapbot.php
